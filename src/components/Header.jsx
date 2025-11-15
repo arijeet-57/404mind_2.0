c0fd6f06a1b8ab65ecf686b84bb6422d19259881
Arijeet Roy <blakelabs57@gmail.com>
2025-11-16 00:57:32 +0530

    Blog1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
         <header style= { {background: "#999",
@@ -5,7 +7,7 @@ function Header() {
       justifyContent: "space-between",
       padding: "0.5rem 1rem"
     }}>
-        <div style={{fontWeight: "bold" ,fontSize: "1.5rem"}}>404minDS_</div>
+        <div style={{fontWeight: "bold" ,fontSize: "1.5rem"}}><Link to="/">404minDS_</Link></div>
         <div style={{textAlign: "right" ,fontSize: "0.9rem"}}>
             <div>Created On: 13/11/25</div>
             <div>Maintained By: Arijeet Roy</div>
